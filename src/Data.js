11ed939e137a34e6dd82884fd6930e2472a3f084
johnkotesh <75444598+johnkotesh@users.noreply.github.com>
2023-06-03 11:53:07 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+import tour1 from './images/tour-1.jpeg';
+import tour2 from './images/tour-2.jpeg';
+import tour3 from './images/tour-3.jpeg';
+
+
+export const pageLinks = [
+    {id: 1, href:'#home', text: 'home' },
+    {id: 2, href:'#about', text: 'about' },
+    {id: 3, href:'#services', text: 'services' },
+    {id: 4, href:'#tours', text: 'tours' },
+];
+
+export const socialLinks = [
+    {id: 1, href: 'https://www.twitter.com', icon:'fab fa-facebook'},
+    {id: 2, href: 'https://www.twitter.com', icon:'fab fa-twitter'},
+    {id: 3, href: 'https://www.twitter.com', icon:'fab fa-squarespace'},
+];
+
+export const services = [
+    {id: 1, icon: 'fas fa-wallet fa-fw', title: 'saving money', text:'Lorem ipsum dolor sit adipisicing'},
+    {id: 2, icon: 'fas fa-wallet fa-fw', title: 'amazing comfort', text:'Lorem ipsum dolor sit adipisicing'},
+    {id: 3, icon: 'fas fa-wallet fa-fw', title: 'good ameneties', text:'Lorem ipsum dolor sit adipisicing'},
+];
+
+export const tours = [
+    {id: 1, image: 'tour1', date:'23rd May 2023', title: 'Tibet Adventure', info:' Lorem ipsum dolor sit amet, consectet', location:'china', duration: 6, cost: 2100,},
+    {id: 2, image: 'tour2', date:'23rd May 2023', title: 'Tibet Adventure', info:' Lorem ipsum dolor sit amet, consectet', location:'china', duration: 6, cost: 2100,},
+    {id: 3, image: 'tour3', date:'23rd May 2023', title: 'Tibet Adventure', info:' Lorem ipsum dolor sit amet, consectet', location:'china', duration: 6, cost: 2100,},
+   
+];
